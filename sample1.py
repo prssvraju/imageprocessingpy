a628464f569b6aeb702e0738d0452627b22e8d6a
prssvraju <prssvraju2@gmail.com>
2023-02-07 22:03:31 +0530

    first commite

---
+++ sample1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from skimage import io
+from matplotlib import pyplot as plt
+import numpy as np
+img = io.imread("/Users/pothuriraju/Documents/imageprocessing/Spinalcord/T1/spint1.jpeg",as_gray=True)
+
+a=np.array([[1,3],[5,6]])
+print(a)
